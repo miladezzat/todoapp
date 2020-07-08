c1f574791eef4009d6bc45815d9a50207865eb35
Milad E. Fahmy <miladezzat.f@gmail.com>
2020-07-08 21:40:27 +0200

    gitignore

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 var mongoose      = require('mongoose');
-//mongodb://<dbuser>:<dbpassword>@ds255309.mlab.com:55309/cars
-var url           = 'mongodb://milad:123@ds255309.mlab.com:55309/cars';
+var url           = 'mongodb://localhost:8001/cars';
 mongoose.Promise = global.Promise;
 mongoose.connect(url)
   .then(() =>  console.log('connection succesful'))
